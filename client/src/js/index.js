279529e518895f0d0b365343a73c49bfddb0870d
Bryan Peens <peensbryan1@gmail.com>
2024-05-11 00:05:52 -0400

    update editor header text

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,9 +2,22 @@ const { Workbox } = require('workbox-window');
 const Editor = require('./editor');
 require('./database');
 require('../css/style.css');
+// Import the header from your JavaScript file
+const { header } = require('./header.js');
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
+
+// Function to append header text to the DOM
+function appendHeaderText() {
+  // Create a new pre element to preserve formatting
+  const headerContainer = document.createElement('pre');
+  // Set the header text as the inner HTML content of the header container
+  headerContainer.innerHTML = header;
+  // Append the header container to the main element
+  main.appendChild(headerContainer);
+}
+
 
 const loadSpinner = () => {
   const spinner = document.createElement('div');
@@ -18,10 +31,13 @@ const loadSpinner = () => {
 };
 
 const editor = new Editor();
+// Call the function to append header text
+appendHeaderText();
 
 if (typeof editor === 'undefined') {
   loadSpinner();
 }
+
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
